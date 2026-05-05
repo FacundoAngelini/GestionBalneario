3bfa7374b13f521749dfc1a343fbb227db959770
Ansbach <107886095+FranCUTN@users.noreply.github.com>
2026-05-05 14:49:17 -0300

    Agregado Handler Global/Subclases

---
+++ src/main/java/com/Gestion/MiBalnearioGestion/Common/Handlers/GlobalExcepcionHandler.java
@@ -0,0 +1,21 @@
+package com.Gestion.MiBalnearioGestion.Common.Handlers;
+
+
+import com.Gestion.MiBalnearioGestion.Common.Exepciones.EntidadNoEncontradaException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExcepcionHandler {
+
+    @ExceptionHandler(EntidadNoEncontradaException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(EntidadNoEncontradaException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(404,"El usuario no fue encontrado : "+ ex.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
--- src/main/java/com/Gestion/MiBalnearioGestion/Reservas/ReservaRepository.java
+++ src/main/java/com/Gestion/MiBalnearioGestion/Reservas/ReservaRepository.java
@@ -3,11 +3,12 @@ package com.Gestion.MiBalnearioGestion.Reservas;
 import com.Gestion.MiBalnearioGestion.Recursos.Entity.RecursoEntity;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Server
+@Repository
 public interface ReservaRepository extends JpaRepository<RecursoEntity, Long> {
 
     boolean existePorIdPublico(UUID idPublico);
