384b878541d9cffbd9a5603728bec1badb1e1dc8
Facundo Ariel Angelini <facundoangelini@gmail.com>
2026-05-11 22:02:35 -0300

    feat:eliminacion de repositorio de direccion, ya que no iba

--- src/main/java/com/Gestion/MiBalnearioGestion/Empleados/Repositorios/DireccionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Gestion.MiBalnearioGestion.Empleados.Repositorios;
-
-import com.Gestion.MiBalnearioGestion.Empleados.Entities.DireccionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DireccionRepository extends JpaRepository<DireccionEntity,Long> {
-
-}
