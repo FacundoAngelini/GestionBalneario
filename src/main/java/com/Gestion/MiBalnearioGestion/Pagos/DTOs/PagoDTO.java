5746dd4ed33a1c89695cc54991e1c87bd7ae81b7
Ansbach <107886095+FranCUTN@users.noreply.github.com>
2026-06-05 12:24:29 -0300

    :nada

--- src/main/java/com/Gestion/MiBalnearioGestion/Pagos/DTOs/PagoDTO.java
+++ src/main/java/com/Gestion/MiBalnearioGestion/Pagos/DTOs/PagoDTO.java
@@ -1,4 +1,43 @@
 package com.Gestion.MiBalnearioGestion.Pagos.DTOs;
 
+import com.Gestion.MiBalnearioGestion.Pagos.Entity.TicketEntity;
+import com.Gestion.MiBalnearioGestion.Pagos.Enum.EestadoPago;
+import com.Gestion.MiBalnearioGestion.Pagos.Enum.MetodoPago;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class PagoDTO {
+
+
+    @NotNull
+    private UUID publicId;
+
+    @NotNull(message = "El monto no puede ser nulo")
+    @Positive(message = "El monto debe ser mayor a 0")
+    @DecimalMin(value = "0.01", message = "El monto mínimo es 0.01")
+    private Double monto;
+
+    @NotNull(message = "El estado de pago no puede ser nulo")
+    private EestadoPago eestadoPago;
+
+    @NotNull(message = "La fecha de pago no puede ser nula")
+    @PastOrPresent(message = "La fecha de pago no puede ser futura")
+    private LocalDate fechaPago;
+
+    @NotNull(message = "El método de pago no puede ser nulo")
+    private MetodoPago metodoPago;
+
+    @DecimalMin(value = "0.0", message = "El descuento no puede ser negativo")
+    @DecimalMax(value = "100.0", message = "El descuento no puede superar el 100%")
+    private Double descuento;  // Opcional, con valor por defecto 0
+
+    @NotNull(message = "El ticket no puede ser nulo")
+    private TicketDTO ticketDTO;
 }
